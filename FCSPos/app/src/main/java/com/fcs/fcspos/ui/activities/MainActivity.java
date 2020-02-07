b7f096c7268c83879ffe0e6e7913749eb0c77b81
HelberKS <helberbr@hotmail.com>
2020-02-07 16:46:47 -0500

    Arquitectura del proyecto nueva para trabajar sobre fragmentos en una sola actividad

--- FCSPos/app/src/main/java/com/fcs/fcspos/ui/activities/MainActivity.java
+++ FCSPos/app/src/main/java/com/fcs/fcspos/ui/activities/MainActivity.java
@@ -1,32 +1,38 @@
 package com.fcs.fcspos.ui.activities;
 
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.fcs.fcspos.R;
 import com.fcs.fcspos.model.OpcionMenu;
+import com.fcs.fcspos.ui.fragments.MenuPrincipalFragment;
 
 
 public class MainActivity extends AppCompatActivity implements OpcionMenu {
 
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
     private String BASE_URL = "http://fcservices.distracom.com.co/TestRestPos/TramaRestService.svc/";
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
-
-
+    private MenuPrincipalFragment menuPrincipalFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipalFrag = new MenuPrincipalFragment();
+        fragmentManager = getSupportFragmentManager();
+        adjuntarFragmentos();
+    }
 
+    private void adjuntarFragmentos() {
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.contMenuPrincipalFrag, menuPrincipalFrag);
+
+
+        fragmentTransaction.show(menuPrincipalFrag);
+        fragmentTransaction.commit();
     }
 
     @Override
